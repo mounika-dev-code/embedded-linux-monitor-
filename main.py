b7adb6e0e4642b007d95b4e4c4d65d23b4b5af5e
Mounika <codes.mounika@gmail.com>
2026-04-28 10:55:16 +0530

    Update main.py
    Adding logging

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 from fastapi import FastAPI, HTTPException
 from services.metrics_service import get_metrics
 
